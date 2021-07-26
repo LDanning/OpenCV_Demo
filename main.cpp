ee0cdca74ceb1934342ec6b8cd66919bb563fb8e
LDanning <xfl063324612@163.com>
2021-07-26 11:48:39 +0800

    OpenCV test demo.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <opencv/cv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    Mat image;
+    image = cv::imread("/home/danning/Pictures/profile.jpg");
+    namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
